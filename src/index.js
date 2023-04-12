63cb361e800384ec2579f6ab10cbe16b9658f69b
Laurent <gauthier.lrnt@gmail.com>
2023-04-12 21:54:14 +0200

    fix: working package

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#! /usr/bin/env node
 import inquirer from 'inquirer'
 import path from 'path'
 import { fileURLToPath } from 'url'
@@ -8,48 +8,52 @@ import fs from 'fs/promises'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      message: 'Select your project type:',
-      name: 'selection',
-      choices: [
-        {
-          name: 'Node.js (prettier + eslint + *ignore)',
-          value: 'nodejs'
-        },
-        {
-          name: 'Node.js typescript',
-          value: 'nodets',
-          disabled: true
-        }
-      ]
-    },
-    {
-      type: 'confirm',
-      name: 'confirmed',
-      message: 'Are you sure?'
-    }
-  ])
-  .then((answers) => {
-    console.log('Answers:', answers)
-    copyDir('src/config/node', 'dev-config')
-    copyDir('src/config/common', 'dev-config')
-  })
-  .catch((error) => {
-    console.error('Error:', error)
-  })
+const run = async () => {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: 'Select your project type:',
+        name: 'selection',
+        choices: [
+          {
+            name: 'Node.js (prettier + eslint + *ignore)',
+            value: 'nodejs'
+          },
+          {
+            name: 'Node.js typescript',
+            value: 'nodets',
+            disabled: true
+          }
+        ]
+      },
+      {
+        type: 'confirm',
+        name: 'confirmed',
+        message: 'Are you sure?'
+      }
+    ])
+    .then((answers) => {
+      console.log('Answers:', answers)
+      copyDir('src/config/node', 'dev-config')
+      copyDir('src/config/common', 'dev-config')
+    })
+    .catch((error) => {
+      console.error('Error:', error)
+    })
 
-// function to copy a directory to a destination
-const copyDir = async (src, dest) => {
-  src = path.join(__dirname, '..', src)
-  dest = path.join(process.cwd(), dest)
-  await fs.mkdir(dest, { recursive: true })
-  let entries = await fs.readdir(src, { withFileTypes: true })
-  for (let entry of entries) {
-    let srcPath = path.join(src, entry.name)
-    let destPath = path.join(dest, entry.name)
-    await fs.copyFile(srcPath, destPath)
+  // function to copy a directory to a destination
+  const copyDir = async (src, dest) => {
+    src = path.join(__dirname, '..', src)
+    dest = path.join(process.cwd(), dest)
+    await fs.mkdir(dest, { recursive: true })
+    let entries = await fs.readdir(src, { withFileTypes: true })
+    for (let entry of entries) {
+      let srcPath = path.join(src, entry.name)
+      let destPath = path.join(dest, entry.name)
+      await fs.copyFile(srcPath, destPath)
+    }
   }
 }
+
+run()
