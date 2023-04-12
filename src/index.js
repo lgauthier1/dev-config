b5d2e3ba09138a398e13de9c681c7eb0e74482cc
Laurent <gauthier.lrnt@gmail.com>
2023-04-12 22:30:31 +0200

    fix: test bump patch 3

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const run = async () => {
-  console.log('test 2')
+  console.log('test 3')
   inquirer
     .prompt([
       {
