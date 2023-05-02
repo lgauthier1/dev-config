db04c8bf844d53038273f87787daa2a84d01d8ac
Laurent <gauthier.lrnt@gmail.com>
2023-05-02 21:49:05 +0200

    fix: remove log

--- src/lib/package.js
+++ src/lib/package.js
@@ -1,10 +1,6 @@
 const mergeKeys = (jsonFile, key, obj) => {
   if (!Object.keys(jsonFile).includes(key)) {
-<<<<<<< HEAD
     // console.warn(`Creation of ${key} in package.json`)
-=======
-    console.warn(`Creation of scripts ${key} in package.json`)
->>>>>>> 8574016 (feat/husky auto setup (#5))
     jsonFile[key] = {}
   }
   jsonFile[key] = { ...jsonFile[key], ...obj }
@@ -20,12 +16,5 @@ export const updatePackage = (jsonFile, scripts = {}, devDependencies = {}) => {
     jsonFile = mergeKeys(jsonFile, 'devDependencies', devDependencies)
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  console.log(jsonFile)
->>>>>>> 8574016 (feat/husky auto setup (#5))
-=======
->>>>>>> 7b14206 (fix: handle json and remove logs)
   return jsonFile
 }
